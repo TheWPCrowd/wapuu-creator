664e6cabe9ba784b1e36c80c7907d864be9e5e0c
Roy Sivan <roy@guavaworks.com>
2016-06-08 07:25:57 -0700

    adding mustaches, and asset_db json

--- assets/js/wapuu-creator.js
+++ assets/js/wapuu-creator.js
@@ -8,15 +8,39 @@ var wapuu_creator = wapuu_creator || {};
         $(document).ready(function(){
             if( $('canvas.wapuu-creator').length > 0 ) {
                 console.log('wapuu creator init');
+
+                /** Event Handling **/
+                $('body').on('click', '.asset-types a', function(e){
+                    e.preventDefault();
+                    var type = $(this).data('type'),
+                        key = $(this).data('key');
+                    creator.load_assets( type, key );
+                });
+
+                /** Create Fabricjs Canvas **/
                 creator.canvas = new fabric.Canvas($('canvas.wapuu-creator').attr('id'));
                 creator.canvas.setDimensions({
-                    width: $(window).width(),
+                    width: $('canvas.wapuu-creator').parents('.content').width(),
                     height: 750
                 });
+
+                /** Insert Base Wapuu **/
                 creator.insertBaseWapuu();
+
+                /** Get Assets **/
+                $.get( wapuuCreatorObj.plugin_url + '/wapuu-assets/asset_db.json', function(res){
+                    creator.assets = res.assets;
+                    $(creator.assets).each(function(key, value){
+                        $('.asset-types').append('<a href="#" data-type="' + value.type + '" data-key="' + key + '">' + value.type + '</a>');
+                    });
+                });
+
             }
         });
 
+        /**
+         * Insert Base Wapuu
+         */
         creator.insertBaseWapuu = function() {
             fabric.Image.fromURL( wapuuCreatorObj.plugin_url + '/wapuu-assets/base-wapuu.svg', function(oImg) {
                 oImg.width = 500;
@@ -29,6 +53,35 @@ var wapuu_creator = wapuu_creator || {};
             /** TESTING ADDING A HAT **/
             creator.insertImage( wapuuCreatorObj.plugin_url + '/wapuu-assets/hats/gradcap.svg', 'hat', {width:400,height:110}, false );
         };
+
+        /**
+         * Load Assets into view
+         * @param type
+         * @param key
+         */
+        creator.load_assets = function( type, key ) {
+            if( !creator.assets || !creator.assets[key] ) {
+                return false;
+            }
+
+            var assets = creator.assets[key];
+            if( type !== assets.type ) {
+                return false;
+            }
+            $('.asset-library').empty();
+
+            $(assets.images).each(function(key, value){
+                $('.asset-library').append('<img src="' + wapuuCreatorObj.plugin_url + '/wapuu-assets/' + assets.type + '/' + value.url + '" data-type="' + assets.type + '" />');
+            });
+        };
+
+        /**
+         * Insert Image onto Fabric.js Canvas
+         * @param image_url
+         * @param image_type
+         * @param image_dimensions
+         * @param image_position
+         */
 
         creator.insertImage = function( image_url, image_type, image_dimensions, image_position ) {
 
--- includes/wapuu-creator-shortcode.php
+++ includes/wapuu-creator-shortcode.php
@@ -6,6 +6,6 @@ class wapuu_creator_shortcode {
 		$a = shortcode_atts( array(
 			'name' => 'wapuu-creator'
 		), $atts);
-		return '<canvas class="wapuu-creator" id="' . $a['name'] . '"></canvas>';
+		return '<div id="wapuu-creator-wrapper"><canvas class="wapuu-creator" id="' . $a['name'] . '"></canvas><div class="row"><div class="col-sm-4 asset-types"></div><div class="col-sm-8 asset-library"></div></div></div>';
 	}
 }
