e0ba13ed19a238b7a0028e475b63e3d732a67d0f
Roy Sivan <roy@guavaworks.com>
2016-06-07 22:05:19 -0700

    adding insertImage function and default positioning based on image type

--- assets/js/wapuu-creator.js
+++ assets/js/wapuu-creator.js
@@ -27,14 +27,35 @@ var wapuu_creator = wapuu_creator || {};
             });
 
             /** TESTING ADDING A HAT **/
-            fabric.Image.fromURL( wapuuCreatorObj.plugin_url + '/wapuu-assets/hats/gradcap.svg', function(oImg) {
-                oImg.width = 400;
-                oImg.height = 110;
+            creator.insertImage( wapuuCreatorObj.plugin_url + '/wapuu-assets/hats/gradcap.svg', 'hat', {width:400,height:110}, false );
+        };
+
+        creator.insertImage = function( image_url, image_type, image_dimensions, image_position ) {
+
+            image_dimensions = image_dimensions || {};
+            image_dimensions.width = image_dimensions.width || 400;
+            image_dimensions.height = image_dimensions.height || 400;
+
+            image_position = image_position || creator.getDefaultPos( image_type );
+
+            fabric.Image.fromURL( image_url, function(oImg) {
+                oImg.width = image_dimensions.width;
+                oImg.height = image_dimensions.height;
                 oImg.set( 'selectable', false );
-                oImg.top = 70;
+                oImg.top = image_position.top;
                 creator.canvas.add(oImg);
                 oImg.centerH();
             });
+        };
+
+        creator.getDefaultPos = function( image_type ) {
+            var dimensions = {};
+            switch( image_type ){
+                case 'hat':
+                    dimensions = { top: 70 }
+                    break;
+            }
+            return dimensions;
         };
 
         creator.downloadJPG = function() {
